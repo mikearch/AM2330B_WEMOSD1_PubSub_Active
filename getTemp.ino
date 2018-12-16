bcaec35222397beacccae4f6becbd334be931b3c
mikearch <mikearch@users.noreply.github.com>
2018-12-16 23:47:30 +0100

    Initial Commit

---
+++ getTemp.ino
@@ -0,0 +1,27 @@
+ //Read data and store it to variables hum and temp
+    
+    void getTemp(){
+   
+  Serial.println("Chip = AM2320");
+  switch(th.Read()) {
+    case 2:
+      Serial.println("  CRC failed");
+      break;
+    case 1:
+      Serial.println("  Sensor offline");
+      break;
+    case 0:
+      Serial.print("  Humidity = ");
+      Serial.print(th.Humidity);
+      hum = (th.Humidity);
+      Serial.println("%");
+      Serial.print("  Temperature = ");
+      Serial.print(th.cTemp);
+      temp = (th.cTemp);
+      Serial.println("*C");
+      Serial.println();
+      break;
+  }
+  //delay(2000);
+  return;
+}
---
+++ pubTemp.ino
@@ -0,0 +1,27 @@
+void pubTemp() {
+
+// Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+   
+    // Read temperature as Celsius (the default)
+  
+   float t = (th.cTemp);
+   
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(t)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+
+    // Computes temperature values in Celsius
+      static char temperatureTemp[7];
+    dtostrf(t, 6, 2, temperatureTemp);   // changed hic variable in this function to t
+    
+    
+
+    // Publishes Temperature and Humidity values
+    client.publish("home/office/esp1/temperature", temperatureTemp);
+    // client.publish("home/office/esp1/humidity", humidityTemp); //comented out humidity stuff
+
+    return;
+}
